f98e0e5135c0af45a37169a972227f27bd9b7941
Dorota Zielinska <dodamad3@gmail.com>
2023-04-02 08:45:01 +0100

    removed old photo and added new one

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <body className="body">
+      <div className="container">
+        <form className="form">
+          <fieldset className="form__fieldset">
+            <legend className="form__legend"><strong>KALKULATOR WALUT</strong></legend>
+            <p>
+              <label> <span className="form__labelText"> Waluta PLN*: </span> <input className="form__field"
+                type="number" name="amount" min="1" step="any" required />
+              </label>
+            </p>
+            <p>
+              <label> <span className="form__labelText">Wybierz Walute:</span>
+                <select className="form__field" name="currency">
+                  <option id="EUR">EUR</option>
+                  <option id="USD">USD</option>
+                  <option id="GBP">GBP</option>
+                </select>
+              </label>
+            </p>
+            <p>
+              <label> <span className="form__labelText"> Wynik:</span>
+                <input className="form__field form__field--special js-result" type="number" name="result" step="any"
+                  disabled />
+              </label>
+            </p>
+          </fieldset>
+          <button className="form__button">Przelicz</button>
+        </form>
+        <p className="container__paragraph">
+          * Wymagane pole; Kurs z dnia 05.02.2023
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+    </body>
   );
 }
 
