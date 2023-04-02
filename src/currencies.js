f0db4b57273ba85d3518a87d3240434abb4e2935
Dorota Zielinska <dodamad3@gmail.com>
2023-04-02 15:43:00 +0100

    added new folder with currency

---
+++ src/currencies.js
@@ -0,0 +1,17 @@
+export const currencies = [
+    {
+        short: "USD",
+        name: "USD",
+        rate: 4.7850,
+    },
+    {
+        short: "EUR",
+        name: "EUR",
+        rate: 4.6850,
+    },
+    {
+        short: "GBP",
+        name: "GBP",
+        rate: 5.5447,
+    },
+];
