cd6bc2ae6e9ea9083e04a1f7035990655e1df2a6
Dorota Zielinska <dodamad3@gmail.com>
2023-04-10 13:19:59 +0100

    change target for sourceAmount

--- src/Form/Result/index.js
+++ src/Form/Result/index.js
@@ -1,11 +1,10 @@
-import React from "react";
 import "./style.css";
 
 export const Result = ({ result }) => (
     <p className="result">
         {result !== undefined && (
             <>
-                {result.targetAmount.toFixed(2)}&nbsp;PLN&nbsp;=&nbsp;
+                {result.sourceAmount.toFixed(2)}&nbsp;PLN&nbsp;=&nbsp;
 
                 <strong>
                     {result.targetAmount.toFixed(2)}&nbsp;{result.currency}
